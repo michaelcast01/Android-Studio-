1a693ac37e5273a4dcb82fa632d40ce443ba51ba
unknown <michael2003cc@gmail.com>
2025-11-17 20:33:30 -0500

    Temporal

--- app/src/main/java/com/example/tiendasuplementacion/screen/CartScreen.kt
+++ app/src/main/java/com/example/tiendasuplementacion/screen/CartScreen.kt
@@ -56,7 +56,7 @@ fun CartScreen(
         }
     }
     
-    // Persistir carrito automáticamente
+    // Persistir carrito automáticamente.
     LaunchedEffect(cartItems) {
         if (cartItems.isNotEmpty()) {
             try {
